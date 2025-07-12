4c839388092231fb4e68e7940e73f2cff1f21387
angel <angelicaizabella90@gmail.com>
2025-07-12 22:36:53 +0700

    add baner to home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Header from '../components/Header'
 import SpecialityMenu from '../components/SpecialityMenu'
 import TopDoctor from '../components/TopDoctor'
+import Banner from '../components/Banner'
+import Footer from '../components/Footer'
 
 
 const Home = () => {
@@ -10,6 +12,8 @@ const Home = () => {
       <Header/>
       <SpecialityMenu/>
       <TopDoctor/>
+      <Banner/>
+      <Footer/>
     </div>
   )
 }
