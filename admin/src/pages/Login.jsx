055dd5b84c0698b10aec8f2ec0a3e3fa8796b5d0
angel <angelicaizabella90@gmail.com>
2025-07-16 15:41:04 +0700

    new folder for admin page (frontend)

---
+++ admin/src/pages/Login.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import { assets } from "../assets/assets";
+
+const Login = () => {
+  const [state, setState] = useState("Admin");
+  
+  return (
+    <form className="min-h-[80vh] flex items-center">
+      <div className="flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96 border-2 rounded-xl text-[#5E5E5E] text-base shadow-lg">
+        <p className="text-2xl font-semibold m-auto">
+          <span className="text-[#5F6FFF]">{state}</span> Login
+        </p>
+        
+        <div className="w-full">
+          <p className="text-base font-medium">Email</p>
+          <input 
+            className='border-2 border-[#DADADA] rounded w-full p-3 mt-1 text-base'
+            type="email" 
+            required 
+          />
+        </div>
+        
+        <div className="w-full">
+          <p className="text-base font-medium">Password</p>
+          <input  
+            className='border-2 border-[#DADADA] rounded w-full p-3 mt-1 text-base' 
+            type="password" 
+            required 
+          />
+        </div>
+        
+        <button className="bg-[#5F6FFF] text-white w-full py-3 rounded-md text-lg font-medium">
+          Login
+        </button>
+        
+        {state === 'Admin'
+          ? <p className="text-base">Doctor Login? <span onClick={() => setState('Doctor')} className="text-[#5F6FFF] cursor-pointer underline font-medium">Click here</span></p>
+          : <p className="text-base">Admin Login? <span onClick={() => setState('Admin')}className="text-[#5F6FFF] cursor-pointer underline font-medium">Click here</span></p>
+        }
+      </div>
+    </form>
+  );
+};
+
+export default Login;
