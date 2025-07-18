fac9e9ef30573e2d08acf7e8c8ffcf71d5242013
angel <angelicaizabella90@gmail.com>
2025-07-18 15:49:25 +0700

    add multipages for admin

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -3,6 +3,12 @@ import Login from './pages/Login'
 import { ToastContainer, toast } from 'react-toastify';
 import { AdminContext } from './context/AdminContext';
 import NavBar from './components/NavBar';
+import SideBar from './components/SideBar';
+import { Route, Routes } from 'react-router-dom';
+import AllAppointment from './pages/Admin/AllAppointment';
+import AddDoctor from './pages/Admin/AddDoctor';
+import DoctorList from './pages/Admin/DoctorList';
+import Dashboard from './pages/Admin/Dashboard';
 
 const App = () => {
   const {aToken} = useContext(AdminContext)
@@ -12,6 +18,16 @@ const App = () => {
     <div className='bg-[#F8F9FD]'>
       <ToastContainer/>
       <NavBar/>
+      <div className='flex items-start'>
+      <SideBar/>
+      <Routes>
+      <Route path='/' element={<></>}/>
+      <Route path='/admin-dashboard' element={<Dashboard/>}/>
+      <Route path='/all-appointment' element={<AllAppointment/>}/>
+      <Route path='/add-doctor' element={<AddDoctor/>}/>
+      <Route path='/doctor-list' element={<DoctorList/>}/>
+      </Routes>
+      </div>
     </div>
   ):(
       // Jika belum login (tidak ada token)
